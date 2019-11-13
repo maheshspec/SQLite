14e7cb762f554c8840f24a67fdfeaa7bcedacc63
saimahesh <saimaheshdhanisetty@gmail.com>
2019-11-13 12:33:31 +0530

    Second Commit

--- app/src/main/java/com/example/sqlitecrud/UserModel.kt
+++ app/src/main/java/com/example/sqlitecrud/UserModel.kt
@@ -4,3 +4,5 @@ import java.io.Serializable
 
 
 data class UserModel(var name:String?=null,var designation:String?=null,var id:Int=0): Serializable
+
+//data class UserModel(var name:String?=null,var designation:String?=null,var id:Int=0): Serializable
